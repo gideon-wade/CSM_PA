5b88b4d04041feb45b6d4a7d3de0e3a78379916e
unknown <s204166@student.dtu.dk>
2022-03-11 10:31:24 +0100

    almost done with task #1

--- GCL.fsx
+++ GCL.fsx
@@ -86,7 +86,43 @@ and evalGCmd (gCmd:gCmd) (mem : Map<string, float>) =
                                 | GCmds(gc1, gc2) -> failwith ("Wrong parse");;
 
 let rec printer (cmd:cmd) =
-    0;;
+    match cmd with
+        | Skip                       -> "Skip"
+        | Assign(x, a)               -> "Assign(" + x + ", " + (printA a) + ")"
+        | AssignArray(x, a1, a2)     -> "Assign(" + x+""+(printA a1)+", "+(printA a2)+")"
+        | Cmds(c1, c2)               -> "Cmds(" + (printer c1) + ", " + (printer c2) + ")"
+        | If(gCmd)                   -> "IFFI(" + (printGc gCmd) + ")"
+        | Do(gCmd)                   -> "DOOD(" + (printGc gCmd) + ")"
+and printA (aexpr : aExpr) =
+    match aexpr with
+        | Num(n) -> sprintf "%f" (n)
+        | Variable(x) -> "VAR("+x+")"
+        | TimesExpr(x,y) -> "Times(" + (printA x) + ", " + (printA y) + ")" 
+        | DivExpr(x,y) -> "Div(" + (printA x) + ", " + (printA y) + ")" 
+        | PlusExpr(x,y) -> "Plus(" + (printA x) + ", " + (printA y) + ")" 
+        | MinusExpr(x,y) -> "Minus(" + (printA x) + ", " + (printA y) + ")" 
+        | PowExpr(x,y) -> "Pow(" + (printA x) + ", " + (printA y) + ")" 
+        | UPlusExpr(x) -> "Pos(" + (printA x) + ")" 
+        | UMinusExpr(x) -> "Neg(" + (printA x) + ")" 
+and printB (bexpr : bExpr) =
+    match bexpr with
+        | True       -> "True"
+        | False      -> "False"
+        | LOr(b1, b2) -> "LOr(" + (printB b1) + ", " + (printB b2) + ")"
+        | LAnd(b1, b2) -> "LAnd(" + (printB b1) + ", " + (printB b2) + ")"
+        | Or(b1, b2) -> "Or(" + (printB b1) + ", " + (printB b2) + ")"
+        | And(b1, b2) -> "And(" + (printB b1) + ", " + (printB b2) + ")"
+        | Not(b) -> "Not(" + (printB b) + ")"
+        | Equals(a1, a2) -> "Eq(" + (printA a1) + ", " + (printA a2) + ")"
+        | NotEquals(a1, a2) -> "NotEq(" + (printA a1) + ", " + (printA a2) + ")"
+        | GrtThan(a1, a2) -> "Grt(" + (printA a1) + ", " + (printA a2) + ")"
+        | GrtEq(a1, a2) -> "GrtEq(" + (printA a1) + ", " + (printA a2) + ")"
+        | LeThan(a1, a2) -> "Le(" + (printA a1) + ", " + (printA a2) + ")"
+        | LeEq(a1, a2) -> "LeEq(" + (printA a1) + ", " + (printA a2) + ")"
+and printGc (gc : gCmd) =
+    match gc with
+        | Statement(bExpr, cmd) -> "Statement(" + (printB bExpr) + ", " + (printer cmd) + ")"
+        | GCmds(gc1, gc2) -> "GCmds(" + printGc gc1 + ", " + printGc gc2 + ")";;
 // We He
 let parse input =
     // translate string into a buffer of characters
@@ -110,7 +146,7 @@ let rec compute n =
             prog <- prog + read
             read <- Console.ReadLine()
         let e = parse (prog)
-        printfn "AST ☢: %A" (e)
+       // printfn "AST ☢ SHEEEEEEEEEEEEEEEEEEEEESH: %A" (e)
         // and print the result of evaluating it 
         //printfn "Result: %A" (evalCmd e Map.empty)
         printfn "Result: %A" (printer e)
